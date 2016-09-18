e3b3b167176e503f60d1b11d2b2067929fb2cd12
Justin Hamilton <justinanthonyhamilton@gmail.com>
2016-09-18 14:16:58 -0700

    Removing color since it is not used in this example.

--- res/shaders/sprite.fs
+++ res/shaders/sprite.fs
@@ -7,5 +7,5 @@ out vec4 color;
 uniform sampler2D ourTexture;
 
 void main() {
-  color = vec4(1.0, 1.0, 1.0, 1.0) * texture(ourTexture, ourTexCoord);
+  color = texture(ourTexture, ourTexCoord);
 }
